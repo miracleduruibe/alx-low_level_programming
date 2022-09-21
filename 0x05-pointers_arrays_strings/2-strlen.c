ca55b401c1121f92067fdc42d9125241833e2aa5
miracleduruibe <miracleduruibe@gmail.com>
2022-09-20 17:15:12 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,13 @@
 /**
- *  * _strlen - return the length of a given string
- *   *
- *    * @s: pointer to string to work on
- *     * Return: string
- *      */
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+ */
 int _strlen(char *s)
 {
-		int len = 0;
+	int count = 0;
 
-			while (*(s + len) != '\0')
-						len++;
-
-				return (len);
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
 }
-Footer
-return
