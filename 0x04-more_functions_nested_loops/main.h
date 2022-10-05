df510d4629d3c9231992136f417e14d27ea1bf09
miracleduruibe <miracleduruibe@gmail.com>
2022-10-05 02:44:12 -0700

    update

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,27 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
-/**
- * main - header file for prototypes
- * void prototypes (void)
- * int prototypes
- */
-
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int _putchar(char c);
 void print_numbers(void);
-void print_diagonal(int n);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
+void num_to_char(unsigned int);
 void print_number(int n);
-int putchar(int _c);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
-void print_numbers(void);
-void print_diagonal(int n);
-int _putchar(int_c);
-_putchar.c
-#endif
+
+#endif /* MAIN_H */
